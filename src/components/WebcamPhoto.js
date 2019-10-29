9a8254c1787d90821b5700c095245d4e2cd2ef34
Antonio Aleman <antonio.9714@gmail.com>
2019-10-29 15:09:14 -0600

    added video as tag instead of component in Home.js

--- src/components/WebcamPhoto.js
+++ src/components/WebcamPhoto.js
@@ -1,34 +1,64 @@
-import React from 'react';
-import Webcam from "react-webcam";
-const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user"
-};
+import React, { Component } from "react";
+import "@tensorflow/tfjs";
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
 
-const WebcamPhoto = () => {
-    const webcamRef = React.useRef(null);
+export default class WebcamComponent extends Component {
 
-    const capture = React.useCallback(
-        () => {
-            const imageSrc = webcamRef.current.getScreenshot();
-        },
-        [webcamRef]
-    );
+    constructor() {
+        super();
+        this.cam = React.createRef();
+    }
 
-    return (
-        <div>
-            <Webcam
-                audio={false}
-                height={360}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                width={640}
-                videoConstraints={videoConstraints}
-            />
-            <button onClick={capture}>Capture photo</button>
-        </div>
-    );
-};
+    // knnLoad = () => {
+    //     //knnClassifier30.json se comparta de manera semi-estable
+    //     // se cambio el 9/10/2019 por knnClassifierv4.json
+    //     //knnClassifierv4.json le falta entrenamiento
+    //     // se dejará por el momento el knnClassiifier30.json
+    //     //let tensorObj = require('./knnClassifier30.json')
+    //     let tensorObj = require('./knnClassifierAndatti.json');
+    //     Object.keys(tensorObj).forEach((key) => {
+    //         console.log(tensorObj[key].length);
+    //         tensorObj[key] = tf.tensor(tensorObj[key], [Math.floor(tensorObj[key].length / 1000), 1024]);
+    //     });
+    //     this.classifier.setClassifierDataset(tensorObj);
+    // }
 
-export default Webcam;
+    // mobilenetLoad = async () => {
+    //     console.log('Loading mobilenet..');
+    //     const net = await mobilenet.load();
+    //     console.log('Sucessfully Mobilnet model');
+    //     this.predictions(net);
+    // }
+
+    setupWebcam = async () => {
+        const node = this.cam.current;
+        return new Promise((resolve, reject) => {
+            const navigatorAny = navigator;
+            navigator.getUserMedia = navigator.getUserMedia ||
+                navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+                navigatorAny.msGetUserMedia;
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia({ video: true },
+                    stream => {
+                        node.srcObject = stream;
+                        node.addEventListener('loadeddata', () => resolve(), false);
+                    },
+                    error => reject());
+            } else {
+                reject();
+            }
+        });
+    }
+
+    componentDidMount() {
+        console.log('DidMount')
+        this.setupWebcam();
+    }
+    render() {
+        return (
+            <video ref={this.cam} autoPlay playsInline muted id="webcam" width="600" height="500" />
+        )
+    }
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,87 +1,137 @@
 import React, { Component } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import "@tensorflow/tfjs";
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
 import "./Home.css";
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isLoading: true,
-      items: []
-    };
-  }
-
-  async componentDidMount() {
-    try {
-      // Extract saved json items
-      const items = this.getItems();
-
-      this.setState({ items });
-    } catch (e) {
-      alert("error: " + e);
+        this.state = {
+            isLoading: true,
+            items: []
+        };
+        this.cam = React.createRef();
+        this.classifier = knnClassifier.create();
     }
 
-    this.setState({ isLoading: false });
-  }
+    knnLoad = () => {
+        //knnClassifier30.json se comparta de manera semi-estable
+        // se cambio el 9/10/2019 por knnClassifierv4.json
+        //knnClassifierv4.json le falta entrenamiento
+        // se dejará por el momento el knnClassiifier30.json
+        //let tensorObj = require('./knnClassifier30.json')
+        let tensorObj = require('../components/knnClassifier.json');
+        Object.keys(tensorObj).forEach((key) => {
+            console.log(tensorObj[key].length);
+            tensorObj[key] = tf.tensor(tensorObj[key], [Math.floor(tensorObj[key].length / 1000), 1024]);
+        });
+        this.classifier.setClassifierDataset(tensorObj);
+    }
 
-  getItems() {
-    // Demo data change for real json generated.
-    return [
-      {
-        itemId: 1,
-        itemName: "Coca Cola",
-        itemQuantity: 1
-      },
-      {
-        itemId: 1,
-        itemName: "Pepsi",
-        itemQuantity: 1
-      }
-    ];
-  }
+    mobilenetLoad = async () => {
+        console.log('Loading mobilenet..');
+        const net = await mobilenet.load();
+        console.log('Sucessfully Mobilnet model');
+        this.predictions(net);
+    }
 
-  renderItemsList(items) {
-    return [{}].concat(items).map((item, i) =>
-      i !== 0 ? (
-        <LinkContainer key={item.itemId} to={`/items/${item.itemId}`}>
-          <ListGroupItem header={item.itemName.trim().split("\n")[0]}>
-            {"Quantity: " + item.itemQuantity}
-          </ListGroupItem>
-        </LinkContainer>
-      ) : null
-    );
-  }
+    setupWebcam = async () => {
+        const node = this.cam.current;
+        return new Promise((resolve, reject) => {
+            const navigatorAny = navigator;
+            navigator.getUserMedia = navigator.getUserMedia ||
+                navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+                navigatorAny.msGetUserMedia;
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia({ video: true },
+                    stream => {
+                        node.srcObject = stream;
+                        node.addEventListener('loadeddata', () => resolve(), false);
+                    },
+                    error => reject());
+            } else {
+                reject();
+            }
+        });
+    }
 
-  renderLander() {
-    return (
-      <div className="lander">
-        <h1>Transfer learning</h1>
-        <p>Product recognition</p>
-      </div>
-    );
-  }
+    async componentDidMount() {
+        try {
+            // Extract saved json items
+            const items = this.getItems();
 
-  renderitems() {
-    return (
-      <div className="items">
-        <PageHeader>Scan new product</PageHeader>
+            this.setState({ items });
+            this.setupWebcam();
+            this.knnLoad();
+            this.mobilenetLoad();
+        } catch (e) {
+            alert("error: " + e);
+        }
 
-        <PageHeader>Shopping cart</PageHeader>
+        this.setState({ isLoading: false });
+    }
 
-        <ListGroup>
-          {!this.state.isLoading && this.renderItemsList(this.state.items)}
-        </ListGroup>
-      </div>
-    );
-  }
+    getItems() {
+        // Demo data change for real json generated.
+        return [
+            {
+                itemId: 1,
+                itemName: "Coca Cola",
+                itemQuantity: 1
+            },
+            {
+                itemId: 1,
+                itemName: "Pepsi",
+                itemQuantity: 1
+            }
+        ];
+    }
 
-  render() {
-    return (
-      <div className="Home">
-        {this.props.isAuthenticated ? this.renderitems() : this.renderLander()}
-      </div>
-    );
-  }
+    renderItemsList(items) {
+        return [{}].concat(items).map((item, i) =>
+            i !== 0 ? (
+                <LinkContainer key={item.itemId} to={`/items/${item.itemId}`}>
+                    <ListGroupItem header={item.itemName.trim().split("\n")[0]}>
+                        {"Quantity: " + item.itemQuantity}
+                    </ListGroupItem>
+                </LinkContainer>
+            ) : null
+        );
+    }
+
+    renderLander() {
+        return (
+            <div className="lander">
+                <h1>Transfer learning</h1>
+                <p>Product recognition</p>
+            </div>
+        );
+    }
+
+    renderitems() {
+        return (
+            <div className="items">
+                <PageHeader>Scan new product</PageHeader>
+                <video ref={this.cam} autoPlay playsInline muted id="webcam" width="600" height="500" />
+                <PageHeader>Shopping cart</PageHeader>
+
+                <ListGroup>
+                    {!this.state.isLoading && this.renderItemsList(this.state.items)}
+                </ListGroup>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="Home">
+                {this.props.isAuthenticated ? this.renderitems() : this.renderLander()}
+            </div>
+        );
+    }
 }
