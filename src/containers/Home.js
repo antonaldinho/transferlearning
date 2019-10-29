44142f5865e1ad9da3a89e7d5e86e6b5f589f06e
HectorNLeon <hectornleon98@gmail.com>
2019-10-29 15:45:53 -0600

    Added classifier for recognizer

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,87 +1,116 @@
 import React, { Component } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import "@tensorflow/tfjs";
 import "./Home.css";
+import Classifier from "../components/Classifier";
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isLoading: true,
-      items: []
-    };
-  }
-
-  async componentDidMount() {
-    try {
-      // Extract saved json items
-      const items = this.getItems();
-
-      this.setState({ items });
-    } catch (e) {
-      alert("error: " + e);
+        this.state = {
+            isLoading: true,
+            items: []
+        };
+        this.cam = React.createRef();
     }
 
-    this.setState({ isLoading: false });
-  }
+    setupWebcam = async () => {
+        const node = this.cam.current;
+        return new Promise((resolve, reject) => {
+            const navigatorAny = navigator;
+            navigator.getUserMedia = navigator.getUserMedia ||
+                navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+                navigatorAny.msGetUserMedia;
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia({ video: true },
+                    stream => {
+                        node.srcObject = stream;
+                        node.addEventListener('loadeddata', () => resolve(), false);
+                    },
+                    error => reject());
+            } else {
+                reject();
+            }
+        });
+    }
 
-  getItems() {
-    // Demo data change for real json generated.
-    return [
-      {
-        itemId: 1,
-        itemName: "Coca Cola",
-        itemQuantity: 1
-      },
-      {
-        itemId: 1,
-        itemName: "Pepsi",
-        itemQuantity: 1
-      }
-    ];
-  }
+    async componentDidMount() {
+        try {
+            // Extract saved json items
+            const items = this.getItems();
 
-  renderItemsList(items) {
-    return [{}].concat(items).map((item, i) =>
-      i !== 0 ? (
-        <LinkContainer key={item.itemId} to={`/items/${item.itemId}`}>
-          <ListGroupItem header={item.itemName.trim().split("\n")[0]}>
-            {"Quantity: " + item.itemQuantity}
-          </ListGroupItem>
-        </LinkContainer>
-      ) : null
-    );
-  }
+            this.setState({ items });
+            this.setupWebcam();
+        } catch (e) {
+            alert("error: " + e);
+        }
 
-  renderLander() {
-    return (
-      <div className="lander">
-        <h1>Transfer learning</h1>
-        <p>Product recognition</p>
-      </div>
-    );
-  }
+        this.setState({ isLoading: false });
+    }
 
-  renderitems() {
-    return (
-      <div className="items">
-        <PageHeader>Scan new product</PageHeader>
+    callbackFunction = (childData) => {
+      
+    }
 
-        <PageHeader>Shopping cart</PageHeader>
+    getItems() {
+        // Demo data change for real json generated.
+        return [
+            {
+                itemId: 1,
+                itemName: "Coca Cola",
+                itemQuantity: 1
+            },
+            {
+                itemId: 1,
+                itemName: "Pepsi",
+                itemQuantity: 1
+            }
+        ];
+    }
 
-        <ListGroup>
-          {!this.state.isLoading && this.renderItemsList(this.state.items)}
-        </ListGroup>
-      </div>
-    );
-  }
+    renderItemsList(items) {
+        return [{}].concat(items).map((item, i) =>
+            i !== 0 ? (
+                <LinkContainer key={item.itemId} to={`/items/${item.itemId}`}>
+                    <ListGroupItem header={item.itemName.trim().split("\n")[0]}>
+                        {"Quantity: " + item.itemQuantity}
+                    </ListGroupItem>
+                </LinkContainer>
+            ) : null
+        );
+    }
 
-  render() {
-    return (
-      <div className="Home">
-        {this.props.isAuthenticated ? this.renderitems() : this.renderLander()}
-      </div>
-    );
-  }
+    renderLander() {
+        return (
+            <div className="lander">
+                <h1>Transfer learning</h1>
+                <p>Product recognition</p>
+            </div>
+        );
+    }
+
+    renderitems() {
+        return (
+            <div className="items">
+                <PageHeader>Scan new product</PageHeader>
+                <video ref={this.cam} autoPlay playsInline muted id="webcam" width="600" height="500" />
+                <Classifier cam={this.cam} parentCallback={this.callbackFunction}></Classifier>
+                <PageHeader>Shopping cart</PageHeader>
+
+                <ListGroup>
+                    {!this.state.isLoading && this.renderItemsList(this.state.items)}
+                </ListGroup>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="Home">
+                {this.props.isAuthenticated ? this.renderitems() : this.renderLander()}
+            </div>
+        );
+    }
 }
